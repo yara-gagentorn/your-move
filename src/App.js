64daa6719affe19dc9cf6d6a2ed6f920886b7a3d
Yara Gagen-Torn <yaragagen@gmail.com>
2023-01-23 21:51:24 +1300

    get info from database

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <AddMove />
+        <MainList />
         {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- src/components/MainList.js
+++ src/components/MainList.js
@@ -1,7 +1,32 @@
-import React, { Component, useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { db } from '../utils/firebase'
+import { ref, onValue } from 'firebase/database'
 
 function MainList() {
-  return ''
+  const [moves, setMoves] = useState([])
+
+  useEffect(() => {
+    onValue(ref(db, 'moves/'), (snapshot) => {
+      setMoves([])
+      const data = snapshot.val()
+      console.log('snapshot', snapshot)
+      if (data !== null) {
+        Object.values(data).map((move) =>
+          setMoves((oldArray) => [...oldArray, move])
+        )
+      }
+    })
+  }, [])
+  return (
+    <>
+      <h1>List here:</h1>
+      {moves.map((move) => (
+        <>
+          <h1>{move.moveName}</h1>
+        </>
+      ))}
+    </>
+  )
 }
 
 export default MainList
